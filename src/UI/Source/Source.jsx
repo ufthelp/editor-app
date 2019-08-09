7caedc786d578d1e59aa8d15ccf0c3ccf7b88623
ufthelp <automationhelpdesk@gmail.com>
2019-08-09 15:38:21 -0400

    AceEditor component

---
+++ src/UI/Source/Source.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import AceEditor from "react-ace";
+
+import "brace/mode/java"; //for rendering language = java in editor
+import "brace/theme/twilight"; //theme variable
+
+import "./Source.scss";
+
+const Source = ({...props}) => (
+  <div className="editorWrapper">
+    <AceEditor {...props} />
+  </div>
+);
+
+export default Source;
--- src/components/RightPanel/RightPanel.jsx
+++ src/components/RightPanel/RightPanel.jsx
@@ -1,53 +1,81 @@
-import React, { memo} from "react";
+import React, { PureComponent } from "react";
 import Tabs from "../Tabs";
-import NetworkGraph from "../../UI/NetworkGraph"
+import NetworkGraph from "../../UI/NetworkGraph";
+import Source from "../../UI/Source";
 import { DataSet } from "vis-network";
 
 import "./RightPanel.scss";
 
 //data to be rendered in Network graph
 const nodes = new DataSet([
-    { id: 1, label: "Node 1" },
-    { id: 2, label: "Node 2" },
-    { id: 3, label: "Node 3" },
-    { id: 4, label: "Node 4" },
-    { id: 5, label: "Node 5" }
-  ]);
-  
-  // create an array with edges
-  const edges = new DataSet([
-    { from: 1, to: 3 },
-    { from: 1, to: 2 },
-    { from: 2, to: 4 },
-    { from: 2, to: 5 }
-  ]);
-  
-  const data = {
-    nodes: nodes,
-    edges: edges
+  { id: 1, label: "Node 1" },
+  { id: 2, label: "Node 2" },
+  { id: 3, label: "Node 3" },
+  { id: 4, label: "Node 4" },
+  { id: 5, label: "Node 5" }
+]);
+
+// create an array with edges
+const edges = new DataSet([
+  { from: 1, to: 3 },
+  { from: 1, to: 2 },
+  { from: 2, to: 4 },
+  { from: 2, to: 5 }
+]);
+
+const data = {
+  nodes: nodes,
+  edges: edges
+};
+const options = {
+  autoResize: true,
+  height: "100%",
+  width: "100%"
+};
+
+class RightPanel extends PureComponent {
+  onChange = data => {
+    console.log(data);
   };
-  const options = {
-    autoResize: true,
-    height: "100%",
-    width: "100%"
-  };
 
+  render() {
+    const sourceProps = {
+      placeholder: "Placeholder Text",
+      mode: "java",
+      theme: "twilight",
+      name: "source",
+      fontSize: "14",
+      highlightActiveLine: true,
+      value: "",
+      width: "100%",
+      height: "100%",
+      setOptions: {
+        enableBasicAutocompletion: false,
+        enableLiveAutocompletion: false,
+        enableSnippets: false,
+        showLineNumbers: true,
+        tabSize: 2
+      },
+      onChange: this.onChange
+    };
 
-const RightPanel = () => (
-  <div className="rightPanelCta">
-    <div className="tab">
-      <Tabs>
-        <div header="Index.jsx">You are on Index.jsx file</div>
-        <div header="Panel.jsx">You are on Panel.jsx file</div>
-
-        <div header="Network Graph">
-          <NetworkGraph data={data} options={options} />
+    return (
+      <div className="rightPanelCta">
+        <div className="tab">
+          <Tabs>
+            <div header="Index.jsx">You are on Index.jsx file</div>
+            <div header="Panel.jsx">You are on Panel.jsx file</div>
+            <div header="Source">
+             <Source {...sourceProps} />
+            </div>
+            <div header="Graph">
+              <NetworkGraph data={data} options={options} />
+            </div>
+          </Tabs>
         </div>
-      </Tabs>
-    </div>
-  </div>
-);
+      </div>
+    );
+  }
+}
 
-
-
-export default memo(RightPanel);
+export default RightPanel;
